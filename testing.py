52842e593ab6fc954dbdef1742cf463c2b05fb60
htmonial <116925658+htmonial@users.noreply.github.com>
2022-11-02 16:24:42 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,17 +1,37 @@
-from datetime import datetime
+import datetime
 
-now = datetime.now()
+now = datetime.datetime.now()
 
-mm = str(now.month)
+print
+print "Current date and time using str method of datetime object:"
+print str(now)
 
-dd = str(now.day)
+print
+print "Current date and time using instance attributes:"
+print "Current year: %d" % now.year
+print "Current month: %d" % now.month
+print "Current day: %d" % now.day
+print "Current hour: %d" % now.hour
+print "Current minute: %d" % now.minute
+print "Current second: %d" % now.second
+print "Current microsecond: %d" % now.microsecond
 
-yyyy = str(now.year)
+print
+print "Current date and time using strftime:"
+print now.strftime("%Y-%m-%d %H:%M")
+The result:
 
-hour = str(now.hour)
+Current date and time using str method of datetime object:
+2013-02-17 16:02:49.338517
 
-mi = str(now.minute)
+Current date and time using instance attributes:
+Current year: 2013
+Current month: 2
+Current day: 17
+Current hour: 16
+Current minute: 2
+Current second: 49
+Current microsecond: 338517
 
-ss = str(now.second)
-
-print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+Current date and time using strftime:
+2013-02-17 16:02
